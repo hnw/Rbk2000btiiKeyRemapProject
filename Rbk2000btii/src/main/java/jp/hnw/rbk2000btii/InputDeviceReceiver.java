06afac53feba6250ab31f118016538f1cf934aaa
Yoshio HANAWA <y@hnw.jp>
2014-02-03 11:44:57 +0900

    first commit

---
+++ Rbk2000btii/src/main/java/jp/hnw/rbk2000btii/InputDeviceReceiver.java
@@ -0,0 +1,12 @@
+package jp.hnw.rbk2000btii;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Nothing to do at this time.
+    }
+}
